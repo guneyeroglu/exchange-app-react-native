478a44c8d566fe9f5303b37370dec8829fd708f1
guneyeroglu <benguneyeroglu@gmail.com>
2023-08-05 00:13:08 +0300

    dailyScreen completed

--- src/screens/DetailScreen/DetailScreen.js
+++ src/screens/DetailScreen/DetailScreen.js
@@ -1,16 +1,72 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
+import { BackIcon } from '../../global/constants/icons';
 import { Layout } from '../../components/Layout';
+import { Loading } from '../../components/Loading';
+
+import { enumScreens } from '../../global/constants/screens';
+import { moneyFormat, dateFormat } from '../../global/utils';
+import { getExchangeRateDetail } from '../../global/services';
 
 import styles from './DetailScreen.style';
 
-export const DetailScreen = () => {
+export const DetailScreen = ({ route }) => {
+  const symbol = route.params.symbol;
+  const navigation = useNavigation();
+
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getDataDetail = async () => {
+    setIsLoading(true);
+
+    const response = await getExchangeRateDetail(symbol);
+
+    if (response) {
+      setData(response);
+    }
+
+    setIsLoading(false);
+  };
+
+  const onPressToHome = () => {
+    navigation.navigate(enumScreens.HOME_SCREEN);
+  };
+
+  useEffect(() => {
+    getDataDetail();
+  }, [symbol]);
+
   return (
-    <Layout>
-      <View style={styles.container}>
-        <Text>DetailScreen</Text>
-      </View>
+    <Layout bottomTabNavigator={false} styles={styles.container}>
+      {!isLoading && (
+        <>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={onPressToHome}>
+              <BackIcon size={36} />
+            </TouchableOpacity>
+            <Text style={styles.title}>{data.name}</Text>
+            <Image source={{ uri: data.icon }} style={styles.headerIcon} />
+          </View>
+          <View style={styles.content}>
+            <View style={styles.tableHead}>
+              <Text style={styles.tableHeadTxt}>Alış</Text>
+              <Text style={styles.tableHeadTxt}>Satış</Text>
+              <Text style={styles.tableHeadTxt}>Tarih</Text>
+            </View>
+            {data?.exchanges?.map((item, index) => (
+              <View key={item.buy * item.sale + item.date} style={[styles.tableRow, index % 2 !== 0 ? styles.tableSecondRow : {}]}>
+                <Text style={[styles.tableRowTxt, styles.firstTxt]}>{`${moneyFormat(item.sale)} ₺`}</Text>
+                <Text style={[styles.tableRowTxt, styles.middleTxt]}>{`${moneyFormat(item.buy)} ₺`}</Text>
+                <Text style={[styles.tableRowTxt, styles.lastTxt]}>{dateFormat(item.date)}</Text>
+              </View>
+            ))}
+          </View>
+        </>
+      )}
+      {isLoading && <Loading />}
     </Layout>
   );
 };
--- src/screens/DetailScreen/DetailScreen.style.js
+++ src/screens/DetailScreen/DetailScreen.style.js
@@ -2,8 +2,73 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
+    paddingHorizontal: 0,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#202124',
+    padding: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    color: '#202124',
+  },
+  headerIcon: {
+    width: 36,
+    height: 36,
+    borderWidth: 1,
+    borderColor: '#202124',
+    borderRadius: 44,
+  },
+  content: {
+    padding: 16,
+  },
+  tableHead: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#202124',
+    padding: 8,
+    borderRadius: 8,
+    marginBottom: 4,
+  },
+  tableHeadTxt: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    color: '#eeeeee',
+  },
+  tableRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 8,
+    backgroundColor: '#20212445',
+    borderRadius: 8,
+    marginBottom: 4,
+  },
+  tableSecondRow: {
+    backgroundColor: '#20212485',
+  },
+  tableRowTxt: {
+    fontSize: 12,
+    fontWeight: 'normal',
+    fontStyle: 'normal',
+    color: '#202124',
+    flex: 1,
+  },
+  firstTxt: {
+    textAlign: 'left',
+  },
+  middleTxt: {
+    textAlign: 'center',
+  },
+  lastTxt: {
+    textAlign: 'right',
   },
 });
