74b976dd6fa9af95b48e3e5b1e2acf65d392dc1e
guneyeroglu <benguneyeroglu@gmail.com>
2023-08-05 00:12:44 +0300

    bottomTabNavigator props changed

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,14 +7,14 @@ import { BottomTabNavigator } from '../BottomTabNavigator';
 
 import styles from './Layout.style';
 
-export const Layout = ({ children, styles: _styles = {} }) => {
+export const Layout = ({ children, bottomTabNavigator = true, styles: _styles = {} }) => {
   const insets = useSafeAreaInsets();
 
   return (
     <View style={[styles.container, { paddingTop: insets.top, paddingBottom: insets.bottom, ..._styles }]}>
       <StatusBar style='auto' />
       {children}
-      <BottomTabNavigator />
+      {bottomTabNavigator && <BottomTabNavigator />}
     </View>
   );
 };
