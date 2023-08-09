14175063ba684d81112787d9a6b595517849623f
guneyeroglu <benguneyeroglu@gmail.com>
2023-08-09 23:37:16 +0300

    new icon added

--- src/global/constants/icons/UserIcon.js
+++ src/global/constants/icons/UserIcon.js
@@ -19,5 +19,3 @@ export const UserIcon = ({ size, color = '#202124' }) => (
     />
   </Svg>
 );
-
-<svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'></svg>;
