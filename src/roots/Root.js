8f20dd449d9b8e6cb9435f6636df834cc767967b
guneyeroglu <benguneyeroglu@gmail.com>
2023-09-03 21:12:48 +0300

    root and auth control changed

--- src/roots/Root.js
+++ src/roots/Root.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ToastProvider } from 'react-native-toast-notifications';
+import * as SplashScreen from 'expo-splash-screen';
 
 import { enumScreens } from '../global/constants/variables/screens';
 import { CustomToast } from '../components/CustomToast';
@@ -10,12 +11,20 @@ import { TabNavigator } from './TabNavigator';
 import { DetailScreen } from '../screens';
 import { ExchangeContext } from '../store';
 
+SplashScreen.preventAutoHideAsync();
+
 export const Root = () => {
   const Stack = createStackNavigator();
   const { handleAuthControl } = useContext(ExchangeContext);
 
   useEffect(() => {
     handleAuthControl();
+
+    const timer = setTimeout(async () => {
+      await SplashScreen.hideAsync();
+    }, 1000);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return (
